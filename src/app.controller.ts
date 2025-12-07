f1244a1608906473165b463d45f546b6e08f22c1
Jixlen999 <viktoriyaivanyutenko@yandex.ru>
2025-12-07 13:47:57 +0300

    feat: setup docker to reload on changes or crash

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/crash')
+  getRoot() {
+    console.log('APP WILL CRASH IN 3 SECONDS');
+
+    setTimeout(() => {
+      console.error('💥💥💥 EXITING WITH CODE 1 💥💥💥');
+      process.exit(1);
+    }, 3000);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  hello() {
-    console.log('hello1');
-  }
-}
+export class AppService {}
